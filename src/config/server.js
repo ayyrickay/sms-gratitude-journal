34b9e815baa56664b551f2902d2e6a1e3dcaf7f3
Ricky <ricky.holtz@berkeley.edu>
2017-08-03 23:16:06 -0700

    Fix environment variables

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-process-env*/
 const config = {
   DEFAULT_PORT: process.env.PORT,
-  MONGODB_URI: process.env.DATABASE_URL
+  MONGODB_URI: process.env.MONGODB_URI
 }
 
 export default config
