494effdb16a721d9aba0934cb6540524650ee088
Ricky Holtz <evholtz@gmail.com>
2017-11-14 18:11:30 -0800

    Dynamically generate hardcoded twiml

--- src/web/message/index.js
+++ src/web/message/index.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser'
 import db from '../../db'
 import express from 'express'
+import twilio from 'twilio'
 
 const router = express.Router()
 
@@ -23,7 +24,11 @@ const parseRequest = (req) => {
 router.post('/', urlEncodedParser, (req, res) => {
   insertEntry(parseRequest(req.body))
 
-  res.send('<Response><Message>Response recorded!</Message></Response>')
+  const twiml = new Twilio(config.TWILIO_SID, config.TWILIO_AUTH).twiml.MessagingResponse
+
+  twiml.message('Response Recorded')
+
+  res.send(twiml.toString())
 })
 
 module.exports = router
